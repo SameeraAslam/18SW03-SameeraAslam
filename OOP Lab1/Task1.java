7ef6d16266b1c20ee4a3e4a8cdaf4abc9d3586cd
SameeraAslam <49363175+SameeraAslam@users.noreply.github.com>
2019-04-07 16:12:28 +0500

    Add files via upload

---
+++ OOP Lab1/Task1.java
@@ -0,0 +1,24 @@
+public class Task1 {
+
+    
+    public static void main(String[] args) {
+        String name ="Sameera";
+        int totalmarks = 400;
+        char grade = 'A';
+        boolean status=true;
+       
+        
+        System.out.println("Name=" +name);
+        System.out.println("Total Marks="+ totalmarks);
+        System.out.println("Grade="+grade);
+         if(status==true){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+        
+        
+    }
+    
+}
---
+++ OOP Lab1/Task2.java
@@ -0,0 +1,13 @@
+ class Task2 {
+
+    public static void main(String[] args) {
+         double d=50.25;
+        int a=(int)d;
+        double f=d-a;
+        System.out.println("Actual Number:"+d);
+        System.out.println("Integral part:"+a);
+        System.out.println("Fractional Part:"+f);
+        
+    }
+    
+}
